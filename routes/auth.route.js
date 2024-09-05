9c9a0a8ccc2ca83cfe8316126d6accc2d1678fac
Danish Ali <sda34495@gmail.com>
2024-09-05 11:00:09 +0500

    frontend

--- routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { adminLogin } from "../controllers/auth.controller.js";
-
-const router = express.Router();
-
-router.post("/login", adminLogin);
-
-export default router;
